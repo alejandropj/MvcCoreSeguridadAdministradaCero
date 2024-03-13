c0fbf09fd025550d3ae4253043194267778f8acc
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-13 11:13:38 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCoreSeguridadAdministradaCero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
